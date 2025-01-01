2e51dd949b9ca67690e8aae59c447d005f820344
maeshu0619 <153809956+maeshu0619@users.noreply.github.com>
2025-01-02 02:50:50 +0900

    Update setup.py

--- system/setup.py
+++ system/setup.py
@@ -2,10 +2,16 @@ from stable_baselines3.common.logger import configure, Logger, TensorBoardOutput
 import datetime
 import os
 
-def file_setup():
+def file_setup(mode):
+    if mode == 0:
+        mode_name = "0_ABR_"
+    elif mode == 1:
+        mode_name = "1_FOCAS_"
+    elif mode == 2:
+        mode_name = "2_A-FOCAS_"
     # ログファイルのセットアップ
     current_time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-    log_file_path = f"log/{current_time}.txt"
+    log_file_path = f"log/{mode_name}{current_time}.txt"
     os.makedirs("log", exist_ok=True)
     log_file = open(log_file_path, "w")
 
@@ -17,3 +23,19 @@ def file_setup():
     ])
 
     return log_file, logger
+
+def extract_bitrate_and_resolution(bitrate_to_resolution):
+    """
+    解像度情報を持つビットレートリストと解像度リストを返す関数。
+
+    Args:
+        bitrate_to_resolution (dict): ビットレートと解像度のマッピング辞書。
+
+    Returns:
+        tuple: 
+            - bitrate_list (list): ビットレートのリスト。
+            - resolution_list (list): 解像度 (横, 縦) のリスト。
+    """
+    bitrate_list = list(bitrate_to_resolution.keys())
+    resolution_list = list(bitrate_to_resolution.values())
+    return bitrate_list, resolution_list
