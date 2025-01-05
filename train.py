39936b80b7d610d4abea1013d4fabf670379d574
maeshu0619 <153809956+maeshu0619@users.noreply.github.com>
2025-01-06 03:13:27 +0900

    Update train.py

--- train.py
+++ train.py
@@ -1,32 +1,28 @@
 
 import argparse
+import time
 from environment.environment import VideoStreamingEnv
 from model.model import DqnAgent
 import numpy as np
+from system.time_calculate import debug_time
+from tqdm import tqdm
 
 def main(mode):
     if mode == 0 or mode == None:
         print("\nMode 0: Training the model with ABR.")
-        num_episodes = 3
+        num_episodes = 5
         max_steps_per_episode = 60
         goal_reward = 800
     elif mode == 1:
         print("\nMode 1: Training the model with FOCAS.")
-        num_episodes = 3
+        num_episodes = 5
         max_steps_per_episode = 60
         goal_reward = 800
     elif mode == 2:
         print("\nMode 2: Training the model with Adaptive-FOCAS.")
-        num_episodes = 3
+        num_episodes = 5
         max_steps_per_episode = 60
         goal_reward = 800
-    '''
-    elif mode == 3:
-        print("\nMode 3: Training the model with ABR-&-FOCAS.")
-        num_episodes = 3
-        max_steps_per_episode = 60
-        goal_reward = 800
-    '''
 
     total_timesteps = 5000
 
@@ -38,9 +34,10 @@ def main(mode):
 
     # トレーニング
     reward_log = []
+    training_cnt = 0
 
     total_timesteps = num_episodes*max_steps_per_episode 
-    #print(f'aaa: {total_timesteps}')
+    
     latency_constraint = 25 * 10**(-3) # レイテンシ制約
 
     # https://qiita.com/sugulu_Ogawa_ISID/items/bc7c70e6658f204f85f9
@@ -50,28 +47,27 @@ def main(mode):
     for episode in range(num_episodes):
         state = env.reset()
         total_reward = 0
-        print(f'episode: {episode}')
 
         for step in range(max_steps_per_episode):
+            training_cnt += 1
+            print(f'step {training_cnt} / {total_timesteps}')
+
             action = agent.actor.get_action(state, episode, agent.q_network)
-            next_state, reward, done, _ = env.step(action)
-            print(f'step; {step+1} / episode: {episode+1}')
+            next_state, reward, done = env.step(action)
 
             agent.memory.add((state, action, reward, next_state, done))
-            if agent.memory.len() > batch_size:
+
+            if agent.memory.len() >= batch_size and agent.memory.len() % 10 == 0:
                 agent.q_network.replay(agent.memory, batch_size, gamma, agent.target_q_network)
 
             state = next_state
             total_reward += reward
-            print(f'reward: {reward}')
 
             if done:
-                print('Loop is broken because of done')
+                #print('Loop is broken because of done')
                 continue
-            print("\n")
 
         reward_log.append(total_reward)
-        print(f"Episode {episode + 1}/{num_episodes} | Total Reward: {total_reward}\n\n")
 
         # ターゲットネットワークの更新
         if episode % 10 == 0:
@@ -79,7 +75,7 @@ def main(mode):
 
         # 終了条件
         if np.mean(reward_log[-10:]) >= goal_reward:
-            print("--- Environment solved! ---")
+            #print("--- Environment solved! ---")
             continue
 
     # モデルの保存
@@ -95,9 +91,15 @@ def main(mode):
 
 # https://qiita.com/sugulu_Ogawa_ISID/items/bc7c70e6658f204f85f9を参照
 if __name__ == "__main__":
+    start_time = time.time()  # 計測開始時刻
     parser = argparse.ArgumentParser(description="Train the model with specified mode.")
     parser.add_argument("--mode", type=int, required=True, help="Specify the mode for training. (e.g., 0 or 1)")
     args = parser.parse_args()
 
     main(args.mode)
+    end_time = time.time()  # 計測終了時刻
+
+    formatted_time = debug_time(end_time-start_time) # デバッグの計算時間の処理
+    print(formatted_time)
+
     
