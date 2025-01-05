30a9d5d50d88db12cf9ea08a6b0229846fdaea70
maeshu0619 <153809956+maeshu0619@users.noreply.github.com>
2025-01-06 03:14:08 +0900

    Update model.py

--- model/model.py
+++ model/model.py
@@ -1,16 +1,22 @@
 import gymnasium
 import gym
+import os
 import numpy as np
+from system.multi_replay import process_replay
+from multiprocessing import Pool
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import Adam
 import tensorflow as tf
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+tf.get_logger().setLevel('ERROR')
 from tensorflow.keras import backend as K
 from collections import deque
 
 # 損失関数の定義
 # 損失関数にhuber関数を使用します
 # 参考: https://github.com/jaara/AI-blog/blob/master/CartPole-DQN.py
+@tf.keras.utils.register_keras_serializable()
 def huberloss(y_true, y_pred):
     err = y_true - y_pred
     cond = K.abs(err) < 1.0
@@ -30,25 +36,21 @@ class QNetwork:
         self.model.compile(loss=huberloss, optimizer=self.optimizer)
 
     def replay(self, memory, batch_size, gamma, targetQN):
-        inputs = np.zeros((batch_size, self.model.input_shape[1]))
-        targets = np.zeros((batch_size, self.model.output_shape[1]))
         states, actions, rewards, next_states, dones = memory.sample(batch_size)
 
+        # 一括予測
+        mainQs = self.model.predict(states)
+        nextMainQs = self.model.predict(next_states)
+        targetQs = targetQN.model.predict(next_states)
         for i in range(batch_size):
-            state_b, action_b, reward_b, next_state_b, done_b = states[i], actions[i], rewards[i], next_states[i], dones[i]
-            inputs[i:i + 1] = state_b
-            target = reward_b
+            if dones[i]:
+                mainQs[i][actions[i]] = rewards[i]
+            else:
+                max_next_action = np.argmax(nextMainQs[i])
+                mainQs[i][actions[i]] = rewards[i] + gamma * targetQs[i][max_next_action]
 
-            if not done_b:
-                retmainQs = self.model.predict(next_state_b[np.newaxis])[0]
-                next_action = np.argmax(retmainQs)
-                target = reward_b + gamma * targetQN.model.predict(next_state_b[np.newaxis])[0][next_action]
-
-            targets[i] = self.model.predict(state_b[np.newaxis])[0]
-            targets[i][action_b] = target
-
-        # verbose=0で進捗バーを抑制
-        self.model.fit(inputs, targets, epochs=1, verbose=0)
+        # 一括トレーニング
+        self.model.fit(states, mainQs, epochs=1, verbose=0)
 
 
 
@@ -62,9 +64,15 @@ class Memory:
 
     def sample(self, batch_size):
         idx = np.random.choice(len(self.buffer), size=batch_size, replace=False)
-        samples = [self.buffer[i] for i in idx]
-        states, actions, rewards, next_states, dones = zip(*samples)  # 各要素を分解
-        return np.array(states), np.array(actions), np.array(rewards), np.array(next_states), np.array(dones)
+        samples = np.array([self.buffer[i] for i in idx], dtype=object)
+        states, actions, rewards, next_states, dones = zip(*samples)
+        return (
+            np.vstack(states),  # 各状態を一括で取得
+            np.array(actions),
+            np.array(rewards),
+            np.vstack(next_states),
+            np.array(dones)
+        )
 
     def len(self):
         return len(self.buffer)
@@ -111,15 +119,6 @@ class Actor:
 
 
 class DqnAgent:
-    """
-    A wrapper class for training and interacting with a DQN agent using Stable Baselines3.
-
-    Args:
-        env (Env): The Gym environment to train and evaluate on.
-        learning_rate (float, optional): The learning rate for the DQN model. Defaults to 0.0001.
-        buffer_size (int, optional): The size of the replay buffer used by the DQN model. Defaults to 1000000.
-    """
-
     def __init__(self, env, mode, learning_rate: float = 0.0001, buffer_size: int = 1000000):
         self.env = env
         self.mode = mode
